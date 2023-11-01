c565fbe587a24b96d7bb2151abb1c428984eeb4f
Huy Trần Lê <tranlehuy0407@gmail.com>
2023-11-01 09:57:23 +0700

    replace

--- Tuan1/src/bai1/Demo.java
+++
@@ -1,8 +0,0 @@
-package bai1;
-
-public class Demo {
-	public static void main(String[] argv) {
-		ToaDo a = new ToaDo("A", 5, 1);
-		System.out.print(a.toString());
-	}
-}
--- Tuan3/src/bai9/Demo.java
+++
@@ -1,9 +0,0 @@
-package bai9;
-
-public class Demo {
-	public static void main(String[] argv) {
-		ToaDo p = new ToaDo("P", 5, 5);
-		HinhTron a = new HinhTron(p, 10.5);
-		System.out.println(a.toString());
-	}
-}
